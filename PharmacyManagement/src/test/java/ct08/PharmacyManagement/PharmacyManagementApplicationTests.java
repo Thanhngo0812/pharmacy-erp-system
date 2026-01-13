55be7d27997e3d08af2f32efbd2edc1b70631a5c
Thanhngo0812 <ngocongthanhsg0812@gmail.com>
2026-01-13 12:39:30 +0700

    Design Project Structure

---
+++ PharmacyManagement/src/test/java/ct08/PharmacyManagement/PharmacyManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package ct08.PharmacyManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharmacyManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
