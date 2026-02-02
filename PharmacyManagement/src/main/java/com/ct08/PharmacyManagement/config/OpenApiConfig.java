af8b84dc3440f8d1bf8d9164d271db2a5bae1c02
Thanhngo0812 <ngocongthanhsg0812@gmail.com>
2026-02-02 20:04:50 +0700

    fix(system):change springboot version

---
+++ PharmacyManagement/src/main/java/com/ct08/PharmacyManagement/config/OpenApiConfig.java
@@ -0,0 +1,25 @@
+package com.ct08.PharmacyManagement.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        final String securitySchemeName = "bearerAuth";
+        return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName)) // Áp dụng global cho tất cả API
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName, new SecurityScheme()
+                                .name(securitySchemeName)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
+    }
+}
