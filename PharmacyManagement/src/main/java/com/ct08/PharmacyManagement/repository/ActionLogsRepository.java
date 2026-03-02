7e7b19305d66177e1157c35105f82bb7a3d05053
Thanhngo0812 <ngocongthanhsg0812@gmail.com>
2026-03-02 14:54:57 +0700

    fix(hr):'solve bug'

--- PharmacyManagement/src/main/java/com/ct08/PharmacyManagement/repository/ActionLogsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ct08.PharmacyManagement.repository;
-
-import com.ct08.PharmacyManagement.entity.ActionLogs;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ActionLogsRepository extends JpaRepository<ActionLogs, Integer> {
-}
